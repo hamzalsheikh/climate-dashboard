30a03d7e3ff58d4259b578dc7475367d50126e7e
hamzalsheikh <7.amza98@gmail.com>
2023-11-29 16:21:39 -0700

    log printing working from one file

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+import os
+from time import sleep, time
+from flask import Flask, render_template, url_for, stream_with_context, request, Response, flash
+from pygtail import Pygtail
+
+app = Flask(__name__)
+
+
+# INDEX 
+@app.route('/')
+def dirtree():
+    path = os.path.expanduser(u'static')
+    return render_template('index.html', tree=make_tree(path))
+
 def make_tree(path):
     tree = dict(name=os.path.basename(path), children=[])
     try: lst = os.listdir(path)
@@ -12,22 +26,10 @@ def make_tree(path):
                 tree['children'].append(dict(name=name))
     return tree
 
-
-import os
-from time import sleep
-from flask import Flask, render_template, url_for
-
-
-app = Flask(__name__)
-
-@app.route('/')
-def dirtree():
-    path = os.path.expanduser(u'static')
-    return render_template('index.html', tree=make_tree(path))
-
+# IMAGES
 @app.route('/<image>')
 def display_image(image):
-    path = os.path.expanduser(u'static')
+    path = os.path.expanduser(u'static/images')
     img = find(image, 'static')
     print(image)
     return render_template('index.html', tree=make_tree(path), image=img)
@@ -37,17 +39,25 @@ def find(name, path):
         if name in files:
             return os.path.join(root, name)
 
-# Log files streaming
+
+# LOGS
 @app.route('/logs')
+def display_log():
+    path = os.path.expanduser(u'static/logs')
+    #log = find(log, 'static/logs')
+    #print(log)
+    return render_template('log.html', tree=make_tree(path))
+
+
+@app.route('/logging-loop')
 def stream():
     def generate():
         with open('test.log') as f:
             while True:
                 yield f.read()
-                #sleep(1)
+                sleep(1)
 
-    return app.response_class(generate(), mimetype='text/plain')
-
+    return app.response_class(generate(), mimetype='text/plain') # render_template('logging.html', logs=generate())
 
 if __name__=="__main__":
-    app.run(host='localhost', port=8888, debug=False)
+    app.run(host='localhost', port=8888, debug=True) 
